10a7f5095dc9292a369f4cfcbb8017ae8e4dbdfa
Ali <av@alivirani.com>
2022-08-21 10:54:37 -0400

    Initial setup

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send('Get API is running correctly')
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
