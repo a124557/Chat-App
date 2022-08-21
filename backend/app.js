05b29c3843648041395185f86d845bbf68772ae7
Ali <av@alivirani.com>
2022-08-21 16:27:54 -0400

    File upadtes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const {table} = require("./data.js");
 
+//Calling dotenv
+dotenv.config()
 const app = express();
 
 app.get("/", (req,res) => {
     res.send('Get API is running correctly')
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.get("/api/chat", (req,res) => {
+    res.send(chats);
+})
+
+app.get("/api/chat/:id", (req,res) => {
+    //Here c refers to a key value in our data table
+    const data = table.find((c) => c.name === req.params.id);
+    res.send(data);
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT)
 });
---
+++ backend/data.js
@@ -0,0 +1,10 @@
+const table = [
+    {name: "test",
+    item: "item1"},
+
+    {name: "test2",
+item: "item2"}
+    
+];
+
+module.exports={table};
