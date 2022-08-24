55e9e33aa8c1ef056ac07237c6a40b595ba7bd07
Ali <av@alivirani.com>
2022-08-24 16:41:01 -0400

    Homepage updates

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,21 +1,74 @@
-import React from "react";
-import { Container, Box } from '@chakra-ui/react'
+import React from 'react';
+import './Home.css';
+import { IconContext } from 'react-icons/lib';
+import { AiOutlineUser } from 'react-icons/ai';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import {
+  Container,
+  Box,
+  Text,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Button,
+  StackDivider,
+} from '@chakra-ui/react';
 
 const Home = () => {
-    return <Container
-    maxW='md'
-    display='flex'
-    justifyContent='center'
-    >
-        <Box 
-        bg='white'
-         w='100%' 
-         p={4} 
-         color='black'>
-  This is the Box
-</Box>
+  return (
+    <Container maxW="lg">
+      <Box
+        bg="rgb(255,255,255,0.95)"
+        w="100%"
+        p={4}
+        color="black"
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems="center"
+        borderRadius="md"
+      >
+        <Stack spacing="4" display="flex" alignItems={'center'}>
+          <Stack spacing={'0'}>
+            <Text fontSize={'4xl'}>Welcome</Text>
+            <Text fontSize={'1xl'}>Please login below</Text>
+          </Stack>
+          <Stack
+            divider={<StackDivider borderColor="gray.400" />}
+            alignItems={'center'}
+            spacing={'5'}
+          >
+            <Stack spacing={'4'}>
+              <InputGroup>
+                <InputLeftElement pointerEvents={'none'} id={'userIcon'}>
+                  <IconContext.Provider value={{ size: '1em' }}>
+                    <div>
+                      <AiOutlineUser />
+                    </div>
+                  </IconContext.Provider>
+                </InputLeftElement>
+                <Input size="lg" placeholder="Username"></Input>
+              </InputGroup>
+              <InputGroup>
+                <InputLeftElement pointerEvents={'none'} id={'passwordIcon'}>
+                  <IconContext.Provider value={{ size: '1em' }}>
+                    <div>
+                      <RiLockPasswordLine />
+                    </div>
+                  </IconContext.Provider>
+                </InputLeftElement>
+                <Input size="lg" placeholder="Password"></Input>
+              </InputGroup>
+              <Button colorScheme={'twitter'}>Sign In</Button>
+            </Stack>
+            <Text fontSize={'sm'}>
+              Don't have an account? <a href="#">Sign up</a>
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
     </Container>
-    
+  );
 };
 
 export default Home;
