6f6294d37efe2700be59fa71d5d77f9aaeb3b742
Ali <av@alivirani.com>
2022-08-29 20:51:36 -0400

    Minor changes to MongoDB model and error handling

--- backend/configuration/mongo.js
+++ backend/configuration/mongo.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const db = async () => {
-  try  {
+  try {
     await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}); 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-
-console.log('Database connected');
-} catch(err) {
-    console.log("Error has occured: ${error.message)");
+    console.log("Database connected");
+  } catch (err) {
+    console.log("Error has occured: " + err);
     process.exit();
-    }   
+  }
 };
 
 module.exports = db;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String, 
-            trim: true, 
-            required: true},
-        email: {
-            type: String, 
-            required: true},
-        password: {
-            type: String, 
-            required: true},
-        picture: {
-            type: String, 
-            required: true,
-        default: "https://img.icons8.com/ios-glyphs/344/user--v1.png"}
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    {timestamps: true}
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+      default: "https://img.icons8.com/ios-glyphs/344/user--v1.png",
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
